54cae96fa7295264549e7a2cbbe2361b47828a1f
Alex Conway <alexconway808@gmail.com>
2014-06-11 21:20:39 -1000

    failed subtraction test

--- spec/mathfunctionsspec.js
+++ spec/mathfunctionsspec.js
@@ -1,11 +1,21 @@
 describe("Math Functions", function() {
 	var result; 
 
+//add function
 
   it("should add numbers properly", function() {
   	result = add(2, 3);
   	expect(result).toBe(5);
 
   });
+
+//subtract function
+
+  it("should subtract numbers properly", function(){
+  	result = subtract(2,3);
+  	expect(result).toBe(5);
+  });
+
+
 
 });	
